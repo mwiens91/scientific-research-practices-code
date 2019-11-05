e73611abb7cf3bfc6f6cabc62fccc1ce54dc9f00
Matt Wiens <mwiens91@gmail.com>
2019-11-05 00:10:10 -0800

    Order constants module alphabetically

--- src/srs-model/model/constants.py
+++ src/srs-model/model/constants.py
@@ -7,14 +7,14 @@ HYPOTHESIS_TALLY = "tally"
 HYPOTHESIS_TRUTH = "truth"
 
 # Result specifiers
-RESULT_POSITIVE = "positive"
 RESULT_NEGATIVE = "negative"
+RESULT_POSITIVE = "positive"
 
 # Investigation constants
 INVESTIGATION_AUTHOR = "author"
-INVESTIGATION_TYPE = "type"
-INVESTIGATION_TRUTH = "truth"
 INVESTIGATION_NOVEL = "novel"
 INVESTIGATION_REPLICATION = "replication"
 INVESTIGATION_RESULT = "result"
 INVESTIGATION_TARGET_HYPOTHESIS = "target"
+INVESTIGATION_TRUTH = "truth"
+INVESTIGATION_TYPE = "type"
